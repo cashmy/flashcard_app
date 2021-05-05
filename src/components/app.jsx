ee0517095e3b0f5e277ba7bd68c02ef907f30d2e
cashmy <cmyers880@gmail.com>
2021-05-05 16:28:12 -0500

    Added Fontawesome Icons

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
 import MiniDrawer from './MiniDrawer/miniDrawer'
 
 
