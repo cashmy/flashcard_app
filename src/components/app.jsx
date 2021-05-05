58bc12d3f2527229e243c98e0b591057e04ab64b
cashmy <cmyers880@gmail.com>
2021-05-05 11:45:51 -0500

    Created basic landing page with Navigation

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import MiniDrawer from './MiniDrawer/miniDrawer'
+
+
+const App = () => {
+
+
+  return (
+    <div >
+      <CssBaseline />
+      <MiniDrawer />
+
+    </div>
+  );
+}
+
+export default App;
