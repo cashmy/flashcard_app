3d5ed261098412f6a8f4504b49fbfda02488b82d
cashmy <cmyers880@gmail.com>
2021-05-06 09:08:45 -0500

    Changed sidebar navigation text & icons

--- src/components/Collections/collectionsTable.jsx
+++ src/components/Collections/collectionsTable.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'id', headerName: 'ID', width: 70, hide: true },
   { field: 'firstName', headerName: 'First name', width: 130 },
   { field: 'lastName', headerName: 'Last name', width: 130 },
   {
@@ -15,7 +15,7 @@ const columns = [
     field: 'fullName',
     headerName: 'Full name',
     description: 'This column has a value getter and is not sortable.',
-    sortable: true,
+    sortable: false,
     width: 160,
     valueGetter: (params) =>
       `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
@@ -37,7 +37,7 @@ const rows = [
 const CollectionsTable = () => {
   return (
     <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+      <DataGrid rows={rows} columns={columns} pageSize={5} />
     </div>
   );
 }
