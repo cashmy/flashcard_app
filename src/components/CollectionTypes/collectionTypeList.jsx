aa94694204ba91fa8b5a9c874b507945e71dc2cc
cashmy <cmyers880@gmail.com>
2021-05-07 15:47:50 -0500

    Added connection to database

--- src/components/CollectionTypes/collectionTypeList.jsx
+++ src/components/CollectionTypes/collectionTypeList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
@@ -8,6 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
 // import tileData from './tileData';
 import image from '../../assets/karen-vardazaryan-JBrfoV-BZts-unsplash.jpg';
+import axios from 'axios'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -25,50 +26,94 @@ const useStyles = makeStyles((theme) => ({
 }));
 
  
- const tileData = [
-   {
-     img: image,
-     title: 'Collection Type',
-     author: 'Cash Myers',
-   },
-   {
-    img: image,
-    title: 'Collection Type 2',
-    author: 'Cash Myers',
-  },
-  {
-    img: image,
-    title: 'Collection Type 3',
-    author: 'Cash Myers',
-  },
-  {
-    img: image,
-    title: 'Collection Type 4',
-    author: 'Cash Myers',
-  },
-  {
-    img: image,
-    title: 'Collection Type 5',
-    author: 'Cash Myers',
-  },
- ];
+// const tileData = mapTileData()
+//  const tileData = [
+//    {
+//      id: 0,
+//      img: image,
+//      title: 'Collection Type',
+//      author: 'Cash Myers',
+//    },
+//    {
+//     id: 1,
+//     img: image,
+//     title: 'Collection Type 2',
+//     author: 'Cash Myers',
+//   },
+//   {
+//     id: 2,
+//     img: image,
+//     title: 'Collection Type 3',
+//     author: 'Cash Myers',
+//   },
+//   {
+//     id: 3,
+//     img: image,
+//     title: 'Collection Type 4',
+//     author: 'Cash Myers',
+//   },
+//   {
+//     id: 4,
+//     img: image,
+//     title: 'Collection Type 5',
+//     author: 'Cash Myers',
+//   },
+//  ];
 
 
 export default function CollectionTypeList() {
   const classes = useStyles();
+  const [collectionTypeData, setCollectionTypeData] = useState({})
+  const [loading, setLoading] = useState(false)
+  
+  useEffect(() => {
+    // if (collectionTypeData === {} || collectionTypeData === undefined ) {
+      setLoading(true);
+      axios.get('http://127.0.0.1:8000/fcCollectionType/')
+      .then(response => { 
+          console.log(response.data)
+          setCollectionTypeData(response.data.items); 
+          setLoading(false)})
+      .catch(error => {alert('There was an error! ' + error.message)})
+    // }
+  },[collectionTypeData])
+  
+  const mapTileData = () => {
+    if (collectionTypeData === '' || collectionTypeData === undefined ) 
+    return ([{
+           id: 0,
+           img: image,
+           title: 'Collection Type',
+           author: 'Cash Myers',
+         }])
+
+    console.log("Mapping ...: ", collectionTypeData)
+    let mapResult = []
+    for (let i = 0; i < collectionTypeData.length; i++) {
+      mapResult.push({
+        id: i,
+        img: image,
+        title: collectionTypeData[i].fcCollectionType_name
+      })
+    }
+    return mapResult
+  }
+  
+  const tileData = mapTileData()
 
   return (
-    <React.Fragment className={classes.root}>
+    <div className={classes.root}>
       <GridList cellHeight={90} cols={1} height="auto" width="auto">
       <GridListTile key="Subheader" cols={1} style={{ height: 'auto' }}>
          <ListSubheader component="div">Collection Types</ListSubheader>
         </GridListTile>
+         if {loading} { <div>Loading ... </div> }
         {tileData.map((tile) => (
-          <GridListTile key={tile.img}>
+          <GridListTile key={tile.id}>
             <img src={tile.img} alt={tile.title} />
             <GridListTileBar
               title={tile.title}
-              subtitle={<span>by: {tile.author}</span>}
+              // subtitle={<span>by: {tile.author}</span>}
               actionIcon={
                 <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
                   <EditIcon fontSize="small" />
@@ -78,6 +123,6 @@ export default function CollectionTypeList() {
           </GridListTile>
         ))}
       </GridList>
-    </React.Fragment>
+    </div>
   );
 }
