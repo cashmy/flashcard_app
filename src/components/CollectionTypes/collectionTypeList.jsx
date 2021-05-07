bdbe67d636974eb25f097a5221222a29f46d79b0
cashmy <cmyers880@gmail.com>
2021-05-07 16:06:03 -0500

    Connctd Collect-Type to db. Rfctrd to class cmpnt

--- src/components/CollectionTypes/collectionTypeList.jsx
+++ src/components/CollectionTypes/collectionTypeList.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
@@ -10,119 +10,61 @@ import EditIcon from '@material-ui/icons/Edit';
 import image from '../../assets/karen-vardazaryan-JBrfoV-BZts-unsplash.jpg';
 import axios from 'axios'
 
-
-const useStyles = makeStyles((theme) => ({
+const styles = theme => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
     overflow: 'hidden',
     backgroundColor: theme.palette.background.paper,
-  },
-
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
-  },
-}));
-
- 
-// const tileData = mapTileData()
-//  const tileData = [
-//    {
-//      id: 0,
-//      img: image,
-//      title: 'Collection Type',
-//      author: 'Cash Myers',
-//    },
-//    {
-//     id: 1,
-//     img: image,
-//     title: 'Collection Type 2',
-//     author: 'Cash Myers',
-//   },
-//   {
-//     id: 2,
-//     img: image,
-//     title: 'Collection Type 3',
-//     author: 'Cash Myers',
-//   },
-//   {
-//     id: 3,
-//     img: image,
-//     title: 'Collection Type 4',
-//     author: 'Cash Myers',
-//   },
-//   {
-//     id: 4,
-//     img: image,
-//     title: 'Collection Type 5',
-//     author: 'Cash Myers',
-//   },
-//  ];
-
-
-export default function CollectionTypeList() {
-  const classes = useStyles();
-  const [collectionTypeData, setCollectionTypeData] = useState({})
-  const [loading, setLoading] = useState(false)
-  
-  useEffect(() => {
-    // if (collectionTypeData === {} || collectionTypeData === undefined ) {
-      setLoading(true);
-      axios.get('http://127.0.0.1:8000/fcCollectionType/')
-      .then(response => { 
-          console.log(response.data)
-          setCollectionTypeData(response.data.items); 
-          setLoading(false)})
-      .catch(error => {alert('There was an error! ' + error.message)})
-    // }
-  },[collectionTypeData])
-  
-  const mapTileData = () => {
-    if (collectionTypeData === '' || collectionTypeData === undefined ) 
-    return ([{
-           id: 0,
-           img: image,
-           title: 'Collection Type',
-           author: 'Cash Myers',
-         }])
-
-    console.log("Mapping ...: ", collectionTypeData)
-    let mapResult = []
-    for (let i = 0; i < collectionTypeData.length; i++) {
-      mapResult.push({
-        id: i,
-        img: image,
-        title: collectionTypeData[i].fcCollectionType_name
-      })
+    '& .MuiButtonBase-root': {
+      color: 'rgba(255, 255, 255, 0.54)',
     }
-    return mapResult
-  }
-  
-  const tileData = mapTileData()
+  },
+});
 
-  return (
-    <div className={classes.root}>
-      <GridList cellHeight={90} cols={1} height="auto" width="auto">
-      <GridListTile key="Subheader" cols={1} style={{ height: 'auto' }}>
-         <ListSubheader component="div">Collection Types</ListSubheader>
-        </GridListTile>
-         if {loading} { <div>Loading ... </div> }
-        {tileData.map((tile) => (
-          <GridListTile key={tile.id}>
-            <img src={tile.img} alt={tile.title} />
-            <GridListTileBar
-              title={tile.title}
-              // subtitle={<span>by: {tile.author}</span>}
-              actionIcon={
-                <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
-                  <EditIcon fontSize="small" />
-                </IconButton>
-              }
-            />
+class CollectionTypeList extends React.Component {
+  state = {
+    collectionTypeData: []
+  }
+
+  componentDidMount() {
+    this.getAllCollectionTypes();
+  }
+
+  async getAllCollectionTypes() {
+    let response = await axios.get('http://127.0.0.1:8000/fcCollectionType/') 
+      this.setState({
+        collectionTypeData: response.data
+      })
+  }
+
+render () {
+  const classes = this.props;
+
+    return (
+      <div className={classes.root}>
+        <GridList cellHeight={90} cols={1} height="auto" width="auto">
+        <GridListTile key="Subheader" cols={1} style={{ height: 'auto' }}>
+          <ListSubheader component="div">Collection Types</ListSubheader>
           </GridListTile>
-        ))}
-      </GridList>
-    </div>
-  );
+          {this.state.collectionTypeData.map((tile) => (
+            <GridListTile key={tile.fcCollectionType_id}>
+              <img src={image} alt={tile.fcCollectionType_name} />
+              <GridListTileBar
+                title={tile.fcCollectionType_name}
+                actionIcon={
+                  <IconButton aria-label={`edit ${tile.fcCollectionType_name}`} >
+                    <EditIcon fontSize="small" />
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    );
+  }
 }
+
+export default withStyles(styles, {withTheme: true})(CollectionTypeList)
--- src/components/Collections/collectionList.jsx
+++ src/components/Collections/collectionList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
@@ -8,75 +8,65 @@ import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
 // import tileData from './tileData';
 import image from '../../assets/martin-adams-zbPDL84kvRg-unsplash.jpg';
+import axios from 'axios'
 
-const useStyles = makeStyles((theme) => ({
+const styles = theme => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
     overflow: 'hidden',
     backgroundColor: theme.palette.background.paper,
+    '& .MuiButtonBase-root': {
+      color: 'rgba(255, 255, 255, 0.54)',
+    }
   },
 
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
-  },
-}));
+});
+class CollectionList extends React.Component {
 
- 
- const tileData = [
-   {
-     img: image,
-     title: 'Collection',
-     author: 'Cash Myers',
-   },
-   {
-    img: image,
-    title: 'Collection 2',
-    author: 'Cash Myers',
-  },
-  {
-    img: image,
-    title: 'Collection 3',
-    author: 'Cash Myers',
-  },
-  {
-    img: image,
-    title: 'Collection 4',
-    author: 'Cash Myers',
-  },
-  {
-    img: image,
-    title: 'Collection 5',
-    author: 'Cash Myers',
-  },
- ];
+  state = {
+    collectionData: []
+  }
 
+  componentDidMount() {
+    this.getAllCollections();
+  }
 
-export default function CollectionList() {
-  const classes = useStyles();
+  async getAllCollections() {
+    let response = await axios.get('http://127.0.0.1:8000/fcCollections/') 
+      this.setState({
+        collectionData: response.data
+      })
+      console.log("Response data:", response.data)
+  }
 
-  return (
-    <React.Fragment className={classes.root}>
-      <GridList cellHeight={90} cols={1} height="auto" width="auto">
-      <GridListTile key="Subheader" cols={1} style={{ height: 'auto' }}>
-         <ListSubheader component="div">Collections</ListSubheader>
-        </GridListTile>
-        {tileData.map((tile) => (
-          <GridListTile key={tile.img}>
-            <img src={tile.img} alt={tile.title} />
-            <GridListTileBar
-              title={tile.title}
-              subtitle={<span>by: {tile.author}</span>}
-              actionIcon={
-                <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
-                  <EditIcon fontSize="small" />
-                </IconButton>
-              }
-            />
+  render(){
+    const classes = this.props;
+
+    return (
+      <div className={classes.root}>
+        <GridList cellHeight={90} cols={1} height="auto" width="auto">
+        <GridListTile key="Subheader" cols={1} style={{ height: 'auto' }}>
+          <ListSubheader component="div">Collections</ListSubheader>
           </GridListTile>
-        ))}
-      </GridList>
-    </React.Fragment>
-  );
+          {this.state.collectionData.map((tile) => (
+            <GridListTile key={tile.fcCollection_id}>
+              <img src={image} alt={tile.fcCollection_title} />
+              <GridListTileBar
+                title={tile.fcCollection_title}
+                actionIcon={
+                  <IconButton aria-label={`edit ${tile.fcCollection_title}`}>
+                    <EditIcon fontSize="small" />
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    );
+  }
 }
+
+export default withStyles(styles, {withTheme: true})(CollectionList)
