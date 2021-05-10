147edbd665dfe3fdcce3e2caeca82da68eb8b3b2
cashmy <cmyers880@gmail.com>
2021-05-10 01:00:22 -0500

    Removed "Strict" mode

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './components/app';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    <App />,
+
   document.getElementById('root')
 );
 
